bd3f71d747fe4fef5db7123d91f7e152a7e955cb
Kamila <kamillkaa95@gmail.com>
2025-07-06 17:23:00 +0300

    app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,57 +5,68 @@ import Form from "../Form/Form";
 import { getPhotos } from "../../services/photos";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import type Photo from "../../types/photo";
-import PhotosGallery from "../PhotosGallery/PhotosGallery";
-import Modal from "../Modal/Modal";
+import type { Photo } from "../../types/photo";
+import PhotoGallery from "../PhotosGallery/PhotosGallery";
+import { Modal } from "../Modal/Modal";
 import Loader from "../Loader/Loader";
+import Text from "../Text/Text";
 
 export default function App() {
-  const [photos, setSelectPhotos] = useState<Photo[]>([]);
+  const [photos, setPhotos] = useState<Photo[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isError, setError] = useState<boolean>(false);
-  const [modalSrc, setModalSrc] = useState<string | null>(null);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [selectedPhoto, setSelectedPhoto] = useState<Photo | null>(null);
 
   const handleSubmit = async (query: string) => {
     try {
       setIsLoading(true);
-      setError(false);
+      setIsError(false);
       setPhotos([]);
+
       const fetchedPhotos = await getPhotos(query);
       if (!fetchedPhotos.length) {
-        toast.error("There are not imgs");
+        toast.error("There are no images found");
         return;
       }
+
       setPhotos(fetchedPhotos);
     } catch {
-      setError(true);
+      setIsError(true);
     } finally {
       setIsLoading(false);
     }
   };
 
   return (
-    <>
-      <Section>
-        <Container>
-          <Form onSubmit={handleSubmit} />
-          {isLoading && <Loader />}
-          {isError && <p>Oops! Something went wrong.</p>}
-          <PhotosGallery photos={photos} onSelect={handleSelect} />
-          {modalSrc && (
-            <Modal onClose={() => setSelectedPhoto(null)}>
-              <div
-                style={{
-                  backgroundColor: selectedPhoto.avg_color,
-                  borderColor: selectedPhoto.avg_color,
-                }}
-              >
-                <img src={selectedPhoto.src.large} alt={selectedPhoto.alt} />
-              </div>
-            </Modal>
-          )}
-        </Container>
-      </Section>
-    </>
+    <Section>
+      <Container>
+        <Form onSubmit={handleSubmit} />
+        {isLoading && <Loader />}
+        {isError && (
+          <Text textAlign="center" marginBottom="60px">
+            Oops! Something went wrong...
+          </Text>
+        )}
+        <PhotoGallery
+          photos={photos}
+          onSelect={(photo) => setSelectedPhoto(photo)}
+        />
+
+        {selectedPhoto && (
+          <Modal onClose={() => setSelectedPhoto(null)}>
+            <div
+              style={{
+                backgroundColor: selectedPhoto.avg_color,
+                borderColor: selectedPhoto.avg_color,
+              }}
+            >
+              <img src={selectedPhoto.src.large} alt={selectedPhoto.alt} />
+            </div>
+          </Modal>
+        )}
+
+        <ToastContainer />
+      </Container>
+    </Section>
   );
 }
